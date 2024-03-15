66bdc5d9ad9d5bb75a1673c90fcbb31c09b16c0d
jojo <perebaj@gmail.com>
2024-03-15 15:33:03 -0300

    :sparkles: feat: day 3

---
+++ leetcode/valid-palindrome/125.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+/*
+65 - 90  UPPER CASE (convert)
+97 - 122  LOWER CASE
+all other ignore
+*/
+func main() {
+	// fmt.Println(byte["a"])
+	s := "A man, a plan, a s canal: Panama"
+	fmt.Println(isPalindrome(s))
+	// fmt.Println(convert(s))
+
+}
+
+func isPalindrome(s string) bool {
+	convertedString := convert(s)
+	initial := 0
+	end := len(convertedString) - 1
+
+	for initial < end {
+		if convertedString[initial] != convertedString[end] {
+			return false
+		}
+		initial++
+		end--
+	}
+	return true
+}
+
+func convert(s string) string {
+	var resp string
+	for _, v := range s {
+		if unicode.IsLetter(v) || unicode.IsNumber(v) {
+			if unicode.IsUpper(v) {
+				resp = resp + string((v + 32))
+			} else {
+				resp = resp + string(v)
+			}
+		}
+	}
+
+	return resp
+}
