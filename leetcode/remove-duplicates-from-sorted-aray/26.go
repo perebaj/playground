af3409463d3501b45d6f10cb5814582cceddb645
jojo <perebaj@gmail.com>
2024-03-17 09:26:02 -0300

    :sparkles: feat: day 5

--- leetcode/remove-duplicates-from-sorted-aray/26.go
+++ leetcode/remove-duplicates-from-sorted-aray/26.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	// fmt.Println(removeDuplicates(nums))
 	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	fmt.Println(removeDuplicates(nums))
+	fmt.Println(removeDuplicates2(nums))
+	nums = []int{1, 1, 2}
+	fmt.Println(removeDuplicates2(nums))
 }
 
 func removeDuplicates(nums []int) int {
@@ -23,4 +26,25 @@ func removeDuplicates(nums []int) int {
 	fmt.Println(nums)
 
 	return total
+}
+
+
+/*
+My first attempt implementing this function was trying to switch the elements
+But the trick here, is think that you
+*/
+func removeDuplicates2(nums []int) int {
+	// var total int
+	position := 0
+	var total int
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[position] && nums[i] > nums[position] {
+			fmt.Println(nums[position], nums[i])
+			position++
+			nums[position] = nums[i]
+			total++
+		}
+	}
+	// fmt.Println(nums)
+	return total + 1
 }
