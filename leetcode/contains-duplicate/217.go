b3bce06feb1c732a6e6c211f9e3959aec62ba458
jojo <perebaj@gmail.com>
2024-03-18 14:59:00 -0300

    :sparkles: dsa: contains duplicate

--- leetcode/contains-duplicate/217.go
+++ leetcode/contains-duplicate/217.go
@@ -3,22 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	in := []int{1, 2, 3, 1}
-	fmt.Println(containsDuplicate(in))
-
+	nums := []int{1, 2, 3, 5}
+	fmt.Println(containsDuplicate(nums))
 }
 
 func containsDuplicate(nums []int) bool {
 	m := make(map[int]int)
-
 	for i := 0; i < len(nums); i++ {
-		m[nums[i]]++
-	}
-
-	for _, v := range m {
-		if v > 1 {
+		_, ok := m[nums[i]]
+		if ok {
 			return true
+		} else {
+			m[nums[i]]++
 		}
 	}
 	return false
 }
+
+/*
+	O(n)
+*/
