b8b5f04952466a7b597d193403b7890fd71ceb74
jojo <perebaj@gmail.com>
2024-03-22 14:08:26 -0300

    :sparkles: leetcode day 10 LRU cache using head and tail// preatty clean implementation

--- leetcode/lru-cache/146.go
+++ leetcode/lru-cache/146.go
@@ -2,186 +2,113 @@ package main
 
 import "fmt"
 
-type DoublyLinkedList struct {
-	Head *Node
-}
-
 type Node struct {
-	Data int
-	Key  int
-	Next *Node
-	Prev *Node
+	key, value int
+	prev, next *Node
 }
 
 type LRUCache struct {
-	DoublyLL DoublyLinkedList
-	HM       map[int]int
-	Capacity int
+	capacity int
+	cache    map[int]*Node
+	head     *Node
+	tail     *Node
 }
 
 func Constructor(capacity int) LRUCache {
+	head := &Node{}
+	tail := &Node{}
+	head.next = tail
+	head.prev = nil
+	tail.prev = head
+	tail.next = nil
 	return LRUCache{
-		DoublyLL: DoublyLinkedList{},
-		HM:       make(map[int]int),
-		Capacity: capacity,
+		capacity: capacity,
+		cache:    map[int]*Node{},
+		head:     head,
+		tail:     tail,
 	}
 }
 
-func (l *LRUCache) isMaxCapacity(key int) bool {
-	_, ok := l.HM[key]
-	if ok {
+func (l *LRUCache) DeleteNode(node *Node) {
+	prev := node.prev
+	next := node.next
+	prev.next = next
+	next.prev = prev
+
+	//delete from hashmap
+	delete(l.cache, node.key)
+}
+
+func (l *LRUCache) AddNode(node *Node) {
+	node.next = l.head.next
+	node.prev = l.head
+	l.head.next.prev = node
+	l.head.next = node
+	//add in Hashmap
+	l.cache[node.key] = node
+}
+
+func (l *LRUCache) Traverse() {
+	current := l.head
+	for current != nil {
+		fmt.Println("traverse", current.key, current.value)
+		current = current.next
+	}
+}
+
+func (l *LRUCache) isMaxCapacity() bool {
+	if len(l.cache) == l.capacity {
+		return true
+	} else {
 		return false
-	} else {
-		return len(l.HM) == l.Capacity
 	}
-}
-
-func (n *DoublyLinkedList) DeleteEnd() int {
-	if n.Head == nil {
-		return -1
-	} else {
-		aux := n.Head
-		for {
-			if aux.Next == nil && aux.Prev != nil { // Deleting the last node
-				prevNode := aux.Prev
-				prevNode.Next = nil
-				return aux.Key
-			} else if aux.Next == nil && aux.Prev == nil {
-				n.Head = nil
-				return aux.Key
-			} else {
-				aux = aux.Next
-			}
-		}
-	}
-
-}
-
-// WasAccessed change
-func (n *DoublyLinkedList) WasAccessed(key int) {
-	if n.Head == nil {
-		return
-	} else {
-		aux := n.Head
-		for aux != nil {
-			if aux.Key == key {
-				prevNode := aux.Prev
-				nextNode := aux.Next
-				if prevNode == nil && nextNode == nil || prevNode == nil {
-					break
-				} else {
-					if nextNode != nil {
-						nextNode.Prev = prevNode
-					}
-					if prevNode != nil {
-						prevNode.Next = nextNode
-					}
-					aux.Prev = nil
-					aux.Next = n.Head
-					n.Head.Prev = aux
-					n.Head = aux
-					return
-				}
-			}
-
-			aux = aux.Next
-		}
-	}
-}
-
-func (n *DoublyLinkedList) InsertBegin(data int, key int) {
-	newNode := Node{
-		Data: data,
-		Prev: nil,
-		Key:  key,
-	}
-	// Verify if the key already exists, if so, update the value
-	aux := n.Head
-	for {
-		if aux == nil {
-			break
-		} else {
-			if aux.Key == key {
-				aux.Data = data
-				n.WasAccessed(key)
-				return
-			}
-			aux = aux.Next
-		}
-	}
-
-	if n.Head == nil {
-		newNode.Next = nil
-		newNode.Prev = nil
-	} else {
-		n.Head.Prev = &newNode
-		newNode.Next = n.Head
-	}
-	n.Head = &newNode
-}
-
-func (n *DoublyLinkedList) traverse() {
-	aux := n.Head
-	for {
-		if aux == nil {
-			break
-		} else {
-			aux = aux.Next
-		}
-	}
-
-}
-
-func (l *LRUCache) Put(key int, value int) {
-	if l.isMaxCapacity(key) {
-		deletedKey := l.DoublyLL.DeleteEnd()
-		if deletedKey == -1 {
-			return
-		} else {
-			delete(l.HM, deletedKey)
-		}
-		// l.DoublyLL.InsertBegin(value, key)
-		// l.HM[key] = value
-	}
-	l.DoublyLL.InsertBegin(value, key)
-	l.HM[key] = value
 }
 
 func (l *LRUCache) Get(key int) int {
-	v, ok := l.HM[key]
-	if !ok {
-		return -1
+	v, ok := l.cache[key]
+	if ok {
+		l.DeleteNode(v)
+		l.AddNode(v)
+		return v.value
 	} else {
-		l.DoublyLL.WasAccessed(key)
-		return v
+		return -1
 	}
 }
 
-func main() {
-	//[[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
+func (l *LRUCache) Put(key, value int) {
+	v, ok := l.cache[key]
+	if ok {
+		l.DeleteNode(v)
+		// newNode := Node{
+		// 	key:   key,
+		// 	value: value,
+		// }
+		// l.AddNode(&newNode)
+	} else if l.isMaxCapacity() {
+		//delete last node
+		lastNode := l.tail.prev
+		l.DeleteNode(lastNode)
+	}
+	newNode := Node{
+		key:   key,
+		value: value,
+	}
+	l.AddNode(&newNode)
 
-	LRUCache := Constructor(3)
-	LRUCache.Put(1, 1)
-	LRUCache.Put(2, 2)
-	LRUCache.Put(3, 3)
-	LRUCache.Put(4, 4)
-	fmt.Println(LRUCache.Get(4))
-	fmt.Println(LRUCache.Get(3))
-	fmt.Println(LRUCache.Get(2))
-	LRUCache.DoublyLL.traverse()
-	fmt.Println(LRUCache.Get(1))
-	LRUCache.Put(5, 5)
-	fmt.Println(LRUCache.HM)
-	fmt.Println(LRUCache.Get(1))
-	fmt.Println(LRUCache.Get(2))
-	fmt.Println(LRUCache.Get(3))
-	fmt.Println(LRUCache.Get(4))
-	fmt.Println(LRUCache.Get(5))
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * obj := Constructor(capacity);
- * param_1 := obj.Get(key);
- * obj.Put(key,value);
- */
+func main() {
+	//[[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
+
+	n := Constructor(2)
+	fmt.Println(n.Get(2))
+	n.Put(2, 6)
+	fmt.Println(n.Get(1))
+	n.Put(1, 5)
+	n.Traverse()
+	n.Put(1, 2)
+	n.Traverse()
+	fmt.Println(n.Get(1))
+	fmt.Println(n.Get(2))
+	n.Traverse()
+}
