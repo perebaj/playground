c4c086e073eae24d796882482b79a30e02caa22a
jojo <perebaj@gmail.com>
2024-04-03 16:13:32 -0300

    :sparkles: leetcode 22

---
+++ leetcode/merge-two-sorted-lists/21.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var res ListNode
+	cur := &res
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
+	}
+
+	return res.Next
+}
