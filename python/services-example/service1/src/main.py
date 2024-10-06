852542ff550e58bdab99f616ad4e1347060bef7b
perebaj <perebaj@gmail.com>
2024-10-05 21:57:30 -0300

    :sparkles: lint

--- python/services-example/service1/src/main.py
+++ python/services-example/service1/src/main.py
@@ -1,15 +1,9 @@
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-
-
 class Service1:
     def __init__(self):
         print("Service1 initialized")
 
     def run(self):
         print(
-            "Service1 running askldjl jaldksj laksj lkjasdkl jalskdj laksdjl ajsdlk jalskdj laksdj lk ajdsljas kldsjalk j kladjs klajsd"
+            "Service1 running askldjl jaldksj laksj lkjasdkl jalskdj laksdjl ajsdlk jalskdj "
+            "laksdj lk ajdsljas kldsjalk j kladjs klajsd"
         )
