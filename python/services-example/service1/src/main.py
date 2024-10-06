6049808d8303e306bb9f5c87e1d0496dc14ff0a3
perebaj <perebaj@gmail.com>
2024-10-05 21:47:49 -0300

    :sparkles: service1

--- python/services-example/service1/src/main.py
+++ python/services-example/service1/src/main.py
@@ -3,3 +3,13 @@ print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
 print("Hello, World!")
+
+
+class Service1:
+    def __init__(self):
+        print("Service1 initialized")
+
+    def run(self):
+        print(
+            "Service1 running askldjl jaldksj laksj lkjasdkl jalskdj laksdjl ajsdlk jalskdj laksdj lk ajdsljas kldsjalk j kladjs klajsd"
+        )
