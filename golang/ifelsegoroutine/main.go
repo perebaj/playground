ff616234d6dcbadcec01ee72c1698c6c9cfcbc96
perebaj <perebaj@gmail.com>
2024-10-16 15:52:41 -0300

    :sparkles: feat: call gourotine if else

---
+++ golang/ifelsegoroutine/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	jjTest("jojo")
+	time.Sleep(time.Second * 1)
+}
+
+func jjTest(s string) {
+	if s == "jojo" {
+		fmt.Println("jojo without goroutine")
+	} else {
+		go func() {
+			fmt.Println("jojo with goroutine")
+		}()
+	}
+
+}
