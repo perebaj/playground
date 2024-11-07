23c2705f4be64538f7414533b8b85dd1f6c7a027
perebaj <perebaj@gmail.com>
2024-11-07 16:30:59 -0300

    :sparkles: xaiohttp

--- python/aiohttp-test/tests/test_xaiohttp.py
+++ python/aiohttp-test/tests/test_xaiohttp.py
@@ -1,7 +1,7 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, AsyncMock
 import pytest
 from xaiohttp import fetch
-
+import asyncio
 
 def test_hello():
     assert 1 == 1
@@ -12,8 +12,20 @@ async def test_fetch(mock_get: MagicMock):
     """Test fetch."""
     mock_get.return_value.__aenter__.return_value.status = 200
     mock_get.return_value.__aenter__.return_value.headers = {'content-type': 'text/html'}
+    #mock the response.json method
+    mock_get.return_value.__aenter__.return_value.json = AsyncMock(return_value={'key': 'value'})
 
-    status, headers = await fetch()
+    status, headers, r_json = await fetch()
 
     assert status == 200
     assert headers == 'text/html'
+    assert r_json == {'key': 'value'}
+
+# @pytest.mark.asyncio
+# @patch('aiohttp.ClientSession.get')
+# async def test_fetch_timeout(mock_get: AsyncMock):
+#     """Test fetch."""
+#     mock_get.__aenter__.get().__aenter__.side_effect = asyncio.TimeoutError
+
+#     with pytest.raises(asyncio.TimeoutError):
+#         status, headers, r_json = await fetch()
--- python/aiohttp-test/xaiohttp.py
+++ python/aiohttp-test/xaiohttp.py
@@ -2,14 +2,20 @@ import aiohttp
 import asyncio
 
 async def fetch():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://python.org') as response:
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(0.02)) as session:
+        try:
+            async with session.get('https://jsonplaceholder.typicode.com/todos/1') as response:
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+                print("Status:", response.status)
+                print("Content-type:", response.headers['content-type'])
 
-            html = await response.text()
-            print("Body:", html[:15], "...")
-            return response.status, response.headers['content-type']
-
+                html = await response.text()
+                r_json = await response.json()
+                print("Body:", html[:15], "...")
+                print("JSON:", r_json)
+                return response.status, response.headers['content-type'], r_json
+        except asyncio.TimeoutError as e:
+            print("TimeoutError:", str(e))
+            print("ConnectionTimeoutError:", str(e))
+            return None, None, None
 asyncio.run(fetch())
