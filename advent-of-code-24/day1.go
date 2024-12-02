f47c750da0ba38fb39fcdea44b4a3a176912acf9
perebaj <perebaj@gmail.com>
2024-12-02 01:37:00 -0300

    :sparkles: advent of code day 1

--- advent-of-code-24/day1.go
+++ advent-of-code-24/day1.go
@@ -12,14 +12,6 @@ import (
 
 func main() {
 	day1()
-
-	// var re = regexp.MustCompile(`(?m)(\d.*)   (\d.*)`)
-	// var str = `40758   64262`
-
-	// match := re.FindStringSubmatch(str)
-	// fmt.Println(match, len(match))
-	// fmt.Println(match[1], match[2])
-
 }
 
 func day1() {
@@ -33,14 +25,12 @@ func day1() {
 	var leftList, rightList []int
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
 		r, _ := regexp.Compile(`(?m)(\d.*)   (\d.*)`)
 		all := r.FindStringSubmatch(scanner.Text())
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		//convert str to int32
 		leftAux, _ := strconv.Atoi(all[1])
 		rightAux, _ := strconv.Atoi(all[2])
 		leftList = append(leftList, leftAux)
@@ -52,16 +42,12 @@ func day1() {
 		fmt.Errorf("%v", err)
 	}
 
-	// fmt.Println(leftList[:2], rightList[:2])
 	sort.Ints(leftList)
 	sort.Ints(rightList)
-	fmt.Println(len(leftList), leftList[:10])
-	fmt.Println(len(rightList), rightList[:10])
 
 	var result int
 	for i := 0; i < len(rightList); i++ {
 		abs := absDistance(rightList[i], leftList[i])
-		fmt.Println(abs)
 		result = result + abs
 	}
 
@@ -74,21 +60,3 @@ func absDistance(a, b int) int {
 	}
 	return a - b
 }
-
-// func main() {
-//     file, err := os.Open("/path/to/file.txt")
-//     if err != nil {
-//         log.Fatal(err)
-//     }
-//     defer file.Close()
-
-//     scanner := bufio.NewScanner(file)
-//     // optionally, resize scanner's capacity for lines over 64K, see next example
-//     for scanner.Scan() {
-//         fmt.Println(scanner.Text())
-//     }
-
-//     if err := scanner.Err(); err != nil {
-//         log.Fatal(err)
-//     }
-// }
