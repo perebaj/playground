e82aa3e6208d00ec254fde7b39e86f75e53399bf
perebaj <perebaj@gmail.com>
2025-01-30 15:44:27 -0300

    :sparkles: post test

--- python/aiohttp-test/tests/test_xaiohttp.py
+++ python/aiohttp-test/tests/test_xaiohttp.py
@@ -1,6 +1,6 @@
 from unittest.mock import patch, MagicMock, AsyncMock
 import pytest
-from xaiohttp import fetch
+from xaiohttp import fetch, post_fetch_example
 import asyncio
 
 
@@ -24,13 +24,27 @@ async def test_fetch(mock_get: MagicMock):
     assert r_json == {'key': 'value'}
 
 
-@pytest.mark.asyncio
-@patch('aiohttp.ClientSession.get')
-async def test_fetch_timeout(mock_get: AsyncMock):
-    """Test fetch."""
-    print(mock_get)
-    # mock_get.side_effect
-    # mock_get.side_effect = asyncio.TimeoutError
+# @pytest.mark.asyncio
+# @patch('aiohttp.ClientSession.get')
+# async def test_fetch_timeout(mock_get: AsyncMock):
+#     """Test fetch."""
+#     print(mock_get)
+#     # mock_get.side_effect
+#     # mock_get.side_effect = asyncio.TimeoutError
 
-    with pytest.raises(asyncio.TimeoutError):
-        status, headers, r_json = await fetch()
+#     with pytest.raises(asyncio.TimeoutError):
+#         status, headers, r_json = await fetch()
+
+
+@pytest.mark.asyncio
+@patch('aiohttp.ClientSession.post')
+async def test_post_fetch_example(mock_post: MagicMock):
+    """Test fetch."""
+    mock_post.return_value.__aenter__.return_value.status = 200
+    mock_post.return_value.__aenter__.return_value.headers = {'content-type': 'text/html', 'Connection': 'close'}
+    # mock the response.json method
+    mock_post.return_value.__aenter__.return_value.json = AsyncMock(return_value={'key': 'value'})
+
+    status, headers, r_json = await
+    print("Debug jj",status, headers, r_json)
+    assert status == 200
--- python/aiohttp-test/xaiohttp.py
+++ python/aiohttp-test/xaiohttp.py
@@ -20,5 +20,16 @@ async def fetch():
             print("ConnectionTimeoutError:", str(e))
             return None, None, None
 
+async def post_fetch_example():
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://jsonplaceholder.typicode.com/posts', json={"title": "foo", "body": "bar", "userId": 1}) as response:
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            r_json = await response.json()
+            print("Body:", html[:15], "...")
+            print("JSON:", r_json)
+            return response.status, response.headers['content-type'], r_json
 
 asyncio.run(fetch())
